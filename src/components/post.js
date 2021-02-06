a20faec53999580a06885b6753d7d5cc7c162fd0
wondwosen shiferaw <wondwosen.shi@gmail.com>
2021-02-06 13:32:38 +0100

    layout font size and background edited

--- src/components/post.js
+++ src/components/post.js
@@ -6,24 +6,24 @@ import "../scss/blog-post.scss"
 
 export default function Post({ title, date, body, fluid, slug }) {
   return (
-    <div className="blog-post-container">
-      <div className="Blog-post">
-        <Img
-          className="post-img"
-          fluid={fluid}
-          objectFit="cover"
-          objectPosition="50% 50%"
-          alt="computer"
-        />
-        <div className="post-title">
-          <h3>{title}</h3>
-          <span>{date}</span>
+    <Link to={`/posts${slug}`} className="readMore">
+      <div className="blog-post-container">
+        <div className="Blog-post">
+          <Img
+            className="post-img"
+            fluid={fluid}
+            objectFit="cover"
+            objectPosition="50% 50%"
+            alt="computer"
+          />
+          <div className="post-title">
+            <h3>{title}</h3>
+            <span>{date}</span>
+          </div>
+          <div>{body && <p>{body}</p>}</div>
         </div>
-        <div>{body && <p>{body}</p>}</div>
+        <button className="readMore">Read More</button>
       </div>
-      <Link to={`/posts${slug}`} className="readMore">
-        Read More
-      </Link>
-    </div>
+    </Link>
   )
 }
--- src/components/sideBarSinglePost.js
+++ src/components/sideBarSinglePost.js
@@ -5,24 +5,23 @@ import { Link } from "gatsby"
 
 export default function sideBarSinglePost({ title, date, fluid, slug }) {
   return (
-    <div className="blog-post-container">
-      <div className="Blog-post">
-        <Img
-          className="post-img"
-          fluid={fluid}
-          objectFit="cover"
-          objectPosition="50% 50%"
-          alt="computer"
-        />
-        <div className="sideBare-title">
-          <h3>{title}</h3>
-          <span>{date}</span>
+    <Link to={`/posts${slug}`}>
+      <div className="blog-post-container">
+        <div className="Blog-post">
+          <Img
+            className="post-img"
+            fluid={fluid}
+            objectFit="cover"
+            objectPosition="50% 50%"
+            alt="computer"
+          />
+          <div className="sideBare-title">
+            <h3>{title}</h3>
+            <span>{date}</span>
+          </div>
+          <button className="readMoreSide">Read More</button>
         </div>
-
-        <Link to={`/posts${slug}`} className="readMoreSide">
-          Read More
-        </Link>
       </div>
-    </div>
+    </Link>
   )
 }
